4e30aec7697b3851e416acfa7eefa7769a4caadb
ParthDalvi164 <parthdalvi164@gmail.com>
2024-06-14 10:35:35 +0530

    pass update

--- timetablegenerator/src/backend/server.js
+++ timetablegenerator/src/backend/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'MySQL123',
+  password: 'admin',
   database: 'reg_portal' // Your database name
 });
 
