e1774801f0b0e744de50521e3da8429a9f6b2668
ParthDalvi164 <parthdalvi164@gmail.com>
2024-06-21 11:59:50 +0530

    logout button

--- timetablegenerator/src/Header.jsx
+++ timetablegenerator/src/Header.jsx
@@ -1,9 +1,13 @@
-// Header.jsx
 import React from 'react';
 import './Header.css';
 import logo from './gstlogo.png';
 
 const Header = () => {
+  const handleLogout = () => {
+    alert("You have been logged out");
+    window.location.reload(); // Refresh the window
+  };
+
   return (
     <div className="header">
       <div className="header-left">
@@ -17,6 +21,7 @@ const Header = () => {
         <h1>SIES Graduate School of Technology</h1>
         <p>www.siesgst.edu.in</p>
         <p>Accredited with NAAC A+ Grade and NBA Accredited</p>
+        <button onClick={handleLogout}>Logout</button>
       </div>
     </div>
   );
